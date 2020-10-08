eccaa6011c5c5bf9ce6a899c4764c9d39ab75438
TCLamnidis <thisseass@gmail.com>
2020-10-08 15:42:27 +0200

    Update to new sidora.core TAB nomenclature

--- query_pandora_for_data.R
+++ query_pandora_for_data.R
@@ -85,34 +85,35 @@ df_list <- get_df_list(
   c("TAB_Organism", "TAB_Protocol", "TAB_Sequencing_Sequencer", "TAB_Site","TAB_Individual","TAB_Library", "TAB_Sequencing","TAB_Raw_Data"),
   con)
 
-# df_list[["TAB_Site"]] <- df_list[["TAB_Site"]] %>% mutate(Site=substr(Full_Library_Id,1,3)) %>% select(Full_Site_Id,Name)
+# df_list[["TAB_Site"]] <- df_list[["TAB_Site"]] %>% mutate(Site=substr(library.Full_Library_Id,1,3)) %>% select(Full_Site_Id,Name)
+# print(str(df_list))
+# print(str(df_list[["TAB_Sequencing_Sequencer"]]))
+df_list[["TAB_Individual"]] <- df_list[["TAB_Individual"]] %>% mutate(Site=substr(individual.Full_Individual_Id,1,3)) %>% select(Site,individual.Full_Individual_Id, individual.Organism)
+df_list[["TAB_Library"]] <- df_list[["TAB_Library"]] %>% mutate(Sample_Name=substr(library.Full_Library_Id,1,6)) %>% select(Sample_Name, library.Full_Library_Id, library.Protocol)
+df_list[["TAB_Sequencing"]] <- df_list[["TAB_Sequencing"]] %>% mutate(Lib=substr(sequencing.Full_Sequencing_Id,1,12), Sequencer=df_list[["TAB_Sequencing_Sequencer"]][["sequencer.Name"]][`sequencing.Sequencer`]) %>% select(Lib, sequencing.Full_Sequencing_Id, Sequencer)
+df_list[["TAB_Raw_Data"]] <- df_list[["TAB_Raw_Data"]]  %>% mutate(Seq=substr(raw_data.Full_Raw_Data_Id,1,18)) %>% select(Seq, raw_data.Full_Raw_Data_Id, raw_data.FastQ_Files)
 
-df_list[["TAB_Individual"]] <- df_list[["TAB_Individual"]] %>% mutate(Site=substr(Full_Individual_Id,1,3)) %>% select(Site,Full_Individual_Id, Organism)
-df_list[["TAB_Library"]] <- df_list[["TAB_Library"]] %>% mutate(Sample_Name=substr(Full_Library_Id,1,6)) %>% select(Sample_Name, Full_Library_Id, Protocol)
-df_list[["TAB_Sequencing"]] <- df_list[["TAB_Sequencing"]] %>% mutate(Lib=substr(Full_Sequencing_Id,1,12), Sequencer=df_list[["TAB_Sequencing_Sequencer"]][["Name"]][`Sequencer`]) %>% select(Lib, Full_Sequencing_Id, Sequencer)
-df_list[["TAB_Raw_Data"]] <- df_list[["TAB_Raw_Data"]]  %>% mutate(Seq=substr(Full_Raw_Data_Id,1,18)) %>% select(Seq, Full_Raw_Data_Id, FastQ_Files)
-
-results <- inner_join(df_list[["TAB_Sequencing"]], query_list_seq, by=c("Full_Sequencing_Id"="Sequencing")) %>% 
-  left_join(., df_list[["TAB_Library"]], by=c("Lib"="Full_Library_Id")) %>%
-  left_join(., df_list[["TAB_Individual"]], by=c("Sample_Name"="Full_Individual_Id")) %>% 
-  mutate(Protocol=map_chr(`Protocol`, function(prot) {df_list[["TAB_Protocol"]] %>% filter(`Id`==prot) %>% .[["Name"]]}),
-         Organism=map_chr(`Organism`, function(org) {df_list[["TAB_Organism"]] %>% filter(`Id`==org) %>% .[["Name"]]})) %>%
-  left_join(., df_list[["TAB_Raw_Data"]], by=c("Full_Sequencing_Id"="Seq")) %>%
+results <- inner_join(df_list[["TAB_Sequencing"]], query_list_seq, by=c("sequencing.Full_Sequencing_Id"="Sequencing")) %>% 
+  left_join(., df_list[["TAB_Library"]], by=c("Lib"="library.Full_Library_Id")) %>%
+  left_join(., df_list[["TAB_Individual"]], by=c("Sample_Name"="individual.Full_Individual_Id")) %>% 
+  mutate(Protocol=map_chr(`library.Protocol`, function(prot) {df_list[["TAB_Protocol"]] %>% filter(`protocol.Id`==prot) %>% .[["protocol.Name"]]}),
+         Organism=map_chr(`individual.Organism`, function(org) {df_list[["TAB_Organism"]] %>% filter(`organism.Id`==org) %>% .[["organism.Name"]]})) %>%
+  left_join(., df_list[["TAB_Raw_Data"]], by=c("sequencing.Full_Sequencing_Id"="Seq")) %>%
   mutate(
-    num_fq=map_int(`FastQ_Files`, function(fq) {ncol(str_split(fq, " ", simplify = T))}), 
-    num_r1=map(`FastQ_Files`, function(fq) {sum(grepl("_R1_",str_split(fq, " ", simplify = T)))}),
+    num_fq=map_int(`raw_data.FastQ_Files`, function(fq) {ncol(str_split(fq, " ", simplify = T))}), 
+    num_r1=map(`raw_data.FastQ_Files`, function(fq) {sum(grepl("_R1_",str_split(fq, " ", simplify = T)))}),
     SeqType=ifelse(num_fq == num_r1, "SE", "PE")) %>%
   select(-starts_with("num_")) %>% 
-  mutate(`FastQ_Files`=map(`FastQ_Files`, function(fq) {str_replace_all(fq, " ([[:graph:]]*_R2_.{3}.fastq.gz)", paste0(";","\\1"))})) %>%
-  separate_rows(`FastQ_Files`, sep=" ") %>%
-  separate(`FastQ_Files`, into=c("R1", "R2"), sep=";", fill="right") %>%
+  mutate(`raw_data.FastQ_Files`=map(`raw_data.FastQ_Files`, function(fq) {str_replace_all(fq, " ([[:graph:]]*_R2_.{3}.fastq.gz)", paste0(";","\\1"))})) %>%
+  separate_rows(`raw_data.FastQ_Files`, sep=" ") %>%
+  separate(`raw_data.FastQ_Files`, into=c("R1", "R2"), sep=";", fill="right") %>%
    mutate(Lane=as.integer(str_replace(`R1`,"[[:graph:]]*_L([[:digit:]]{3})_R[[:graph:]]*", "\\1")), 
           Strandedness=map_chr(`Protocol`, function (.) {infer_library_specs(.)[1]}), 
           UDG_Treatment=map_chr(`Protocol`, function(.){infer_library_specs(.)[2]}), 
           Colour_Chemistry=map_int(`Sequencer`, infer_color_chem),
           BAM=NA
           ) %>%
-  rename(Library_ID=Full_Sequencing_Id)
+  rename(Library_ID=sequencing.Full_Sequencing_Id)
 
 if (!is.na(args[3]) && args[3] == "--debug") {
   write_tsv(results, "Debug_table.txt")
