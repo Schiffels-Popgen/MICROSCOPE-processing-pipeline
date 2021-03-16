24e9c3ef69315c53c77462f4fc33e44c8a631e39
TCLamnidis <thisseass@gmail.com>
2021-03-16 15:48:20 +0100

    Added singularity to profile specificiation. needed since profile definition updates

--- run_Eager.sh
+++ run_Eager.sh
@@ -6,7 +6,7 @@ microscope_config='/projects1/MICROSCOPE/MICROSCOPE.config'
 
 ## Set profiles based on cluster.
 if [[ $(hostname) =~ ^mpi- ]]; then
-	nextflow_profiles="shh"
+	nextflow_profiles="shh,singularity"
 elif [[ $(hostname) =~ ^cdag ]]; then
 	nextflow_profiles="cdag,shh"
 fi
