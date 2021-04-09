86898fb23b67bef972233c46fbd7253c6885e953
TCLamnidis <thisseass@gmail.com>
2021-04-09 14:37:15 +0200

    added microscope profile to config. Added 'Yes to all' option for resuming runs to wrapper.

--- run_Eager.sh
+++ run_Eager.sh
@@ -6,7 +6,7 @@ microscope_config='/projects1/MICROSCOPE/MICROSCOPE.config'
 
 ## Set profiles based on cluster.
 if [[ $(hostname) =~ ^mpi- ]]; then
-	nextflow_profiles="shh,singularity"
+	nextflow_profiles="shh,singularity,microscope"
 elif [[ $(hostname) =~ ^cdag ]]; then
 	nextflow_profiles="cdag,shh"
 fi
@@ -32,7 +32,7 @@ for eager_input in /projects1/MICROSCOPE/eager_inputs/*.eager_input.tsv; do
             --email ${USER}@shh.mpg.de \
             --outdir ${eager_output_dir} \
             -w ${eager_output_dir}/work \
-            #-name ${run_name}"
+            -with-tower"
 
             nextflow run nf-core/eager \
                 -r ${eager_version} \
@@ -42,41 +42,48 @@ for eager_input in /projects1/MICROSCOPE/eager_inputs/*.eager_input.tsv; do
                 --email ${USER}@shh.mpg.de \
                 --outdir ${eager_output_dir} \
                 -w ${eager_output_dir}/work \
-                #-name ${run_name}
+                -with-tower
+            
+            touch ${eager_output_dir} ## Refresh the creation date of the output directory to reflect the end of the new run.
 
-    ## If the directory exists but lacks a MultiQC ask to attempt to resume execution. Helpful if a run failed.
-    elif [[ ! -f ${eager_output_dir}/multiqc/multiqc_report.html ]]; then
-        unset user_reply
-        echo -e "${Yellow}Output directory for batch ${Red}$(basename ${eager_output_dir})${Yellow} already exists, but lacks 'multiqc/multiqc_report.html'.'$(tput sgr0)" ## '$(tput sgr0) returns to normal printing after the line is done
-        echo "If a nextflow run for that batch did not complete successfully and was killed, I can try to resume that run from where it failed."
-        echo "Would you like me to try? [y/n]"
-        read user_reply
+    ## If the MultiQC report is older than the directory, or doesnt exist yet, try to resume execution. Helpful for runs that failed.
+    elif [[ ${eager_output_dir} -nt ${eager_output_dir}/multiqc/multiqc_report.html ]]; then
+        if [[ user_reply != "Y" ]]; then 
+            unset user_reply
+            echo -e "${Yellow}Output directory for batch ${Red}$(basename ${eager_output_dir})${Yellow} already exists, but lacks 'multiqc/multiqc_report.html', or that report is outdated.'$(tput sgr0)" ## '$(tput sgr0) returns to normal printing after the line is done
+            echo "If a nextflow run for that batch did not complete successfully and was killed, I can try to resume that run from where it failed."
+            echo """Would you like me to try?
+            [y]es
+            [n]o
+            [Y]es to all"""
+            read user_reply
+        fi
         ## Ensure user reply is in expected format. Only "y" or "n" allowed.
-        while ! [[ "${user_reply}" =~ ^(y|n)$ ]]; do
-            echo "Unrecognised input. [y/n]"
+        while ! [[ "${user_reply}" =~ ^(y|n|Y)$ ]]; do
+            echo "Unrecognised input. [y/n/Y]"
             read user_reply
         done
-        if [[ ${user_reply} == "y" ]]; then
+        if [[ ${user_reply} =~ ^(y|Y)$ ]]; then
             echo "nextflow run nf-core/eager \
                 -r ${eager_version} \
-                -profile shh,sdag \
+                -profile ${nextflow_profiles} \
                 -c ${microscope_config} \
                 --input ${eager_input} \
                 --email ${USER}@shh.mpg.de \
                 --outdir ${eager_output_dir} \
                 -w ${eager_output_dir}/work \
-                #-name ${run_name} \
+                -with-tower \
                 -resume"
             
             nextflow run nf-core/eager \
                 -r ${eager_version} \
-                -profile shh,sdag \
+                -profile ${nextflow_profiles} \
                 -c ${microscope_config} \
                 --input ${eager_input} \
                 --email ${USER}@shh.mpg.de \
                 --outdir ${eager_output_dir} \
                 -w ${eager_output_dir}/work \
-                #-name ${run_name} \
+                -with-tower \
                 -resume
         else
             echo "OK! ${eager_input} was skipped"
