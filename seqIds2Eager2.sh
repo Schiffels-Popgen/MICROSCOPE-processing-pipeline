e46e10f03f3d61fea354fd78b91d3b9ea0a4f54a
Thiseas C. Lamnidis <thisseass@gmail.com>
2021-09-09 13:47:48 +0200

    Compatibility with EVA cluster. Path updates

--- seqIds2Eager2.sh
+++ seqIds2Eager2.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
-DIR="/projects1/MICROSCOPE"
+DIR="/mnt/archgen/MICROSCOPE"
+
+cred_file="~thiseas_christos_lamnidis/Software/github/Schiffels-Popgen/MICROSCOPE-processing-pipeline/.credentials"
 
 for seq_batch in ${DIR}/sequencing_batches/*; do
     fn_eager_input="${DIR}/eager_inputs/$(basename ${seq_batch} .txt).eager_input.tsv"
     if [[ ${seq_batch} -nt ${fn_eager_input} ]]; then
         echo "Now processing ${seq_batch}"
-        ~lamnidis/Software/Pipelining/query_pandora_for_data.R ${seq_batch} ~lamnidis/Software/Pipelining/.credentials | \
-            sed 's/\/mnt\/archgen\/data_releases/\/projects1\/data_releases_leipzig/g' \
+        ~thiseas_christos_lamnidis/Software/github/Schiffels-Popgen/MICROSCOPE-processing-pipeline/query_pandora_for_data.R ${seq_batch} ${cred_file} \
             >${DIR}/eager_inputs/$(basename ${seq_batch} .txt).eager_input.tsv
     else
         echo "${seq_batch} has already been processed"
