943730ba966d3086719df90af124e45fd3f000a8
Thiseas C. Lamnidis <thisseass@gmail.com>
2021-11-11 15:13:59 +0100

    Gather site Ids

--- make_poseidon_packages.sh
+++ make_poseidon_packages.sh
@@ -42,3 +42,31 @@ for seq_batch in $(find /mnt/archgen/MICROSCOPE/eager_outputs/* -maxdepth 0 ! -p
     fi
 done
 
+## Gather all site ids
+cat /mnt/archgen/MICROSCOPE/poseidon_packages/*/*ind.txt | cut -c1-3 >/mnt/archgen/MICROSCOPE/poseidon_packages/Sites.txt
+
+## Construct list of site names, lat and lon from pandora
+Rscript /mnt/archgen/MICROSCOPE/site_ids_to_names.R
+
+## Set the population field in ind file to the Site ID
+for ind_f in /mnt/archgen/MICROSCOPE/poseidon_packages/*/*ind.txt; do
+    awk -v OFS="\t" -F "\t" '{$3=substr($1,1,3); print $0}' ${ind_f} >${ind_f}.2
+    mv ${ind_f} ${ind_f}.old
+    mv ${ind_f}.2 ${ind_f}
+done
+
+## Add Site_ID and Site_Name to .janno
+for janno_f in /mnt/archgen/MICROSCOPE/poseidon_packages/*/*.janno; do
+    temp_janno="$(dirname ${janno_f})/temp_janno"
+    head -n1 ${janno_f} > ${temp_janno}
+    tail -n +2 ${janno_f} | awk -F '\t' -v OFS='\t' '{$19=substr($1,1,3); print $0}' >> ${temp_janno}
+
+    temp_site_list="$(dirname ${janno_f})/temp_site_list"
+    echo -e "ID\tSite\tLatitude\tLongitude" > ${temp_site_list}
+    while read r; do grep $r poseidon_packages/Sites_Info.txt ; done < <(awk '{print $19}' ${temp_janno}) >>${temp_site_list}
+    paste ${temp_site_list} ${temp_janno} | awk -F '\t' -v OFS='\t' '{$10=$2; $11=$3; $12=$4; print $0}' | cut -f 5- >${temp_janno}_2
+    mv ${janno_f} ${janno_f}.old
+    mv ${temp_janno}_2 ${janno_f}
+    rm $(dirname ${janno_f})/temp_*
+done
+
