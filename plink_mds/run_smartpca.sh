b79b6afe0e25dd150949c6dfbacea1bc9c6df9e0
Thiseas C. Lamnidis <thisseass@gmail.com>
2021-12-14 15:59:10 +0100

    Script to run smartpca if needed

---
+++ plink_mds/run_smartpca.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+input_geno=$1
+input_snp=$2
+input_ind=$3
+poplist=$4
+cpus=$5
+
+## Only run if the bed file has been updated
+if [[ $(readlink -f ${input_geno}) -nt ${output_dir}/West_Eurasian_pca.evec ]]; then
+    echo -e "genotypename: ${input_geno}" >smartpca.par
+    echo -e "snpname: ${input_snp}" >>smartpca.par
+    echo -e "indivname: ${input_ind}" >>smartpca.par
+    echo -e "evecoutname: West_Eurasian_pca.evec" >>smartpca.par
+    echo -e "evaloutname: West_Eurasian_pca.eval" >>smartpca.par
+    echo -e "poplistname: ${poplist}" >>smartpca.par
+    echo -e "outliermode: 2" >>smartpca.par
+    echo -e "numthreads: ${cpus}" >>smartpca.par
+    echo -e "lsqproject: YES" >>smartpca.par
+    echo -e "autoshrink: YES" >>smartpca.par
+    smartpca -p smartpca.par
+else
+    echo "No changes in the 'microscope_pca' package since the distance matrix was computed."
+    echo "Halting execution"
+    exit 0
+fi
