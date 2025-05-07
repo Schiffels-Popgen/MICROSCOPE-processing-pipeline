e5598c22123e74d91102d7ca27fe2ad4b57161c4
Thiseas Christos Lamnidis <thisseass@gmail.com>
2025-05-07 14:05:11 +0200

    specify trident path

--- plink_mds/forge_pca_package.sh
+++ plink_mds/forge_pca_package.sh
@@ -29,6 +29,7 @@ forge_dir="/mnt/archgen/MICROSCOPE/forged_packages/"
 package_dir="${forge_dir}/microscope_pca/"
 we_poplist='/home/thiseas_christos_lamnidis/Software/github/Schiffels-Popgen/MICROSCOPE-processing-pipeline/plink_mds/west_eurasian_poplist.txt'
 pca_dir="/mnt/archgen/MICROSCOPE/microscope_pca"
+trident_path="/home/thiseas_christos_lamnidis/anaconda3/envs/MICROSCOPE/bin/trident"
 
 ## Do not remake the package if it does not need updating, unless forced to with -f.
 jannos=(${poseidon_dir}/*/*.janno)
@@ -55,8 +56,8 @@ if [[ ${force_remake} == "TRUE" || ${newest_janno} -nt /mnt/archgen/MICROSCOPE/f
         tail -n +2 ${janno} | awk -F "\t" -v OFS='\t' '{print $3}' | sort -u >> ${forge_dir}/forgelist.txt
     done
 
-    trident forge --forgeFile ${forge_dir}/forgelist.txt -d ${poseidon_dir} -o ${package_dir} -n microscope_pca --intersect --eigenstrat
-    trident genoconvert -d ${package_dir} --outFormat PLINK
+    ${trident_path} forge --forgeFile ${forge_dir}/forgelist.txt -d ${poseidon_dir} -o ${package_dir} -n microscope_pca --intersect --eigenstrat
+    ${trident_path} genoconvert -d ${package_dir} --outFormat PLINK
 else
     echo "No changes in any janno files since the 'microscope_pca' package was created."
     echo "Halting execution."
